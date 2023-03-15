8d18e594046cebe34e3adde150281c46c289a665
Gnosis-hash <31098668+Gnosis-hash@users.noreply.github.com>
2023-03-15 19:19:09 -0400

    Update listy.py

--- listy.py
+++ listy.py
@@ -1,4 +1,3 @@
-# Define your image and string dictionary
 data = {
      "image1": {
      "src":"https://media.tenor.com/xIjVfKFLarEAAAAC/babies-dance.gif",
@@ -130,4 +129,269 @@ data = {
      "src":"https://media.tenor.com/9CJaHEmyKPAAAAAC/chris-pratt-andy-dwyer.gif",
      "alt":"placeholder",
      "text":"You surprising and tricking me with the escape room!"
-  }}
+  },
+
+     "image23": {
+     "src":"https://media.tenor.com/zUKBSZNpM-IAAAAC/betty-white-happy-new-years.gif",
+     "alt":"placeholder",
+     "text":"New years <3, the guy who said 'She loves you man! She loves you!' when you fixed my hair for the photo."
+  },
+
+     "image24": {
+     "src":"https://media.tenor.com/XwxyX-fTBXYAAAAM/mean-girls-your-hair-looks-sexy-pushed.gif",
+     "alt":"placeholder",
+     "text":"Everytime you 'fix' my hair, but it's always the same."
+  },
+
+     "image25": {
+     "src":"https://media.tenor.com/feiX6p7p0oQAAAAM/running-mascot.gif",
+     "alt":"placeholder",
+     "text":"Us trying to get on the top of the parking garage where the fireworks were set!"
+  },
+
+     "image26": {
+     "src":"https://media.tenor.com/G_93pif7k8YAAAAC/busted-police.gif",
+     "alt":"placeholder",
+     "text":"The police catching us at the airport...he he..."
+  },
+
+     "image27": {
+     "src":"https://media.tenor.com/wsEE0-pBdyYAAAAM/mtg-cat.gif",
+     "alt":"placeholder",
+     "text":"Playing that card game in the back seat of your car."
+  },
+
+     "image28": {
+     "src":"https://media.tenor.com/a0vLaXP700EAAAAC/the-simpsons-phone.gif",
+     "alt":"placeholder",
+     "text":"Us always having something to talk about for hours."
+  },
+
+     "image29": {
+     "src":"https://media.tenor.com/CVmxXO22ITIAAAAC/akirambow-spoiled-rabbit.gif",
+     "alt":"placeholder",
+     "text":"Sleeping together on the phone."
+  },
+
+     "image30": {
+     "src":"https://media.tenor.com/z8bhIjinEYoAAAAC/crying-tears.gif",
+     "alt":"placeholder",
+     "text":"You making me name 20 things that are good about me. My favorite memory."
+  },
+
+     "image31": {
+     "src":"https://media.tenor.com/pvrtnxVI76AAAAAM/you-really-believe-in-me-eric-cartman.gif",
+     "alt":"placeholder",
+     "text":"You always believing in me."
+  },
+
+     "image32": {
+     "src":"https://media.tenor.com/sFg4Db4w3TMAAAAC/i%27m-great%21-i%27m-better-thangreat.-i-am-good.-matt-leblanc.gif",
+     "alt":"placeholder",
+     "text":"You always thinking of the good in me even when I doubt myself."
+  },
+
+     "image33": {
+     "src":"https://media.tenor.com/pq6lPeaWc1IAAAAM/rabbit-bunny.gif",
+     "alt":"placeholder",
+     "text":"Praying at the zoo and at the hotel."
+  },
+
+     "image34": {
+     "src":"https://media.tenor.com/ds3MkTKYd5IAAAAC/car-jump.gif",
+     "alt":"placeholder",
+     "text":"You taking me to the auto show where I finally got to see so many of my favorite cars."
+  },
+
+     "image35": {
+     "src":"https://media.tenor.com/Y8An9OjsIaUAAAAM/ford-bronco-bronco.gif",
+     "alt":"placeholder",
+     "text":"You telling the Ford driver 'whichever option is the safest' before we went down hill."
+  },
+
+     "image36": {
+     "src":"https://media.tenor.com/4hgLFtGX1lYAAAAM/explain-plan.gif",
+     "alt":"placeholder",
+     "text":"You planning the last trip even though it's something that's tough for you."
+  },
+
+     "image37": {
+     "src":"https://media.tenor.com/sY5J9DLdLhYAAAAM/holding-hands.gif",
+     "alt":"placeholder",
+     "text":"Us wandering around CVS just to spend more time with each other."
+  },
+
+     "image38": {
+     "src":"https://media.tenor.com/6RC5V-cKs4gAAAAC/xmas-christmas-gift.gif",
+     "alt":"placeholder",
+     "text":"Watching you open the first set of gifts I got you at the Amazon Fresh."
+  },
+
+     "image39": {
+     "src":"https://media.tenor.com/PQdSSmWOOksAAAAM/comeback-cz-tomi-paci.gif",
+     "alt":"placeholder",
+     "text":"Pravda?!"
+  },
+
+     "image40": {
+     "src":"https://media.tenor.com/98LBqCtWLckAAAAM/pastor-preaching.gif",
+     "alt":"placeholder",
+     "text":"In the beginging."
+  },
+
+     "image41": {
+     "src":"https://media.tenor.com/w6DS9_ISMc0AAAAM/baby-cute.gif",
+     "alt":"placeholder",
+     "text":"you lnuv me? you lnuvvv ME?!"
+  },
+
+     "image42": {
+     "src":"https://media.tenor.com/vx84yXy5NX8AAAAM/grin-face-grin.gif",
+     "alt":"placeholder",
+     "text":"Your cheeky smile giphy."
+  },
+
+     "image43": {
+     "src":"https://media.tenor.com/TsCnU3sRPnAAAAAd/isaac-barduch-innappropriate-laugh.gif",
+     "alt":"placeholder",
+     "text":"Me: 'My ex was a paralegal too.' You: 'Oh ya? Which one?'"
+  },
+
+     "image44": {
+     "src":"https://media.tenor.com/anU9mW455VUAAAAM/cat-frank-the-cat.gif",
+     "alt":"placeholder",
+     "text":"Billi instantly letting you pet him."
+  },
+
+     "image45": {
+     "src":"https://media.tenor.com/IAfCisC5F40AAAAd/your-voice-is-like-butter-to-our-ears-casey-miller.gif",
+     "alt":"placeholder",
+     "text":"The first time I heard your voice on the phone."
+  },
+
+     "image46": {
+     "src":"https://media.tenor.com/cy5s0ovvhyIAAAAM/love-bugs-bunny.gif",
+     "alt":"placeholder",
+     "text":"Everytime I hear your voice on the phone."
+  },
+
+     "image47": {
+     "src":"https://media.tenor.com/eCKbp68flHgAAAAd/hey-babies.gif",
+     "alt":"placeholder",
+     "text":"Us talking about having 15+ babies."
+  },
+
+     "image48": {
+     "src":"https://media.tenor.com/EDRj8kgdZIIAAAAC/lauren-london-on-the-phone.gif",
+     "alt":"placeholder",
+     "text":"Everytime I realize I've been talking for so long and feel bad, but you always say something like 'no it's ok, I like hearing your voice'."
+  },
+
+     "image49": {
+     "src":"https://media.tenor.com/_j8hn150YlwAAAAM/look.gif",
+     "alt":"placeholder",
+     "text":"The way you look at me."
+  },
+
+     "image50": {
+     "src":"https://media.tenor.com/3_bylZubyPoAAAAC/face-palm-shake-my-head.gif",
+     "alt":"placeholder",
+     "text":"You saying 'oy'."
+  },
+
+     "image51": {
+     "src":"https://media.tenor.com/56j46_YxVKwAAAAd/stephenvlog-stephen-georg.gif",
+     "alt":"placeholder",
+     "text":"The way you say your Rs and Bs."
+  },
+
+     "image52": {
+     "src":"https://media.tenor.com/UG9UwbKsN5gAAAAM/dog-doggo.gif",
+     "alt":"placeholder",
+     "text":"Your willingness to help no matter the situation."
+  },
+
+     "image53": {
+     "src":"https://media.tenor.com/6wOT1ybuZUMAAAAd/thirsty-drinking.gif",
+     "alt":"placeholder",
+     "text":"Whenever you remind me to drink water."
+  },
+
+     "image54": {
+     "src":"https://media.tenor.com/61bEKACH460AAAAM/scratching-cat-head.gif",
+     "alt":"placeholder",
+     "text":"The way you scratch my head."
+  },
+
+     "image55": {
+     "src":"https://media.tenor.com/2ZM8Q4VsDboAAAAM/hello-liz-lemon.gif",
+     "alt":"placeholder",
+     "text":"The way you answer the phone 'h hullo...?'"
+  },
+
+     "image56": {
+     "src":"https://media.tenor.com/1wffTU8qRCYAAAAM/sport-success.gif",
+     "alt":"placeholder",
+     "text":"The way you do lovingly kept being persistent on going somewhere else for the ultrasound."
+  },
+
+     "image57": {
+     "src":"https://media.tenor.com/9qn_nI927TYAAAAM/ateam-plan.gif",
+     "alt":"placeholder",
+     "text":"Making plans together when I'm coming."
+  },
+
+     "image58": {
+     "src":"https://media.tenor.com/Z01wKd337ssAAAAM/sponge-bob-thinking.gif",
+     "alt":"placeholder",
+     "text":"Picking out your food for you."
+  },
+
+     "image59": {
+     "src":"https://media.tenor.com/_HCFHg4cssIAAAAS/shoes-slipper.gif",
+     "alt":"placeholder",
+     "text":"Putting your shoes and jacket in when you're leaving."
+  },
+
+     "image60": {
+     "src":"https://media.tenor.com/ozYvHd-6Dm8AAAAC/couple-car.gif",
+     "alt":"placeholder",
+     "text":"Holding your hand while driving."
+  },
+
+     "image61": {
+     "src":"https://media.tenor.com/V1i3tRl7LdsAAAAM/kidnap-cat-kidnap.gif",
+     "alt":"placeholder",
+     "text":"Finding cute babies and telling each other and planning on stealing them."
+  },
+
+     "image62": {
+     "src":"https://media.tenor.com/bBdYX_BgAokAAAAM/gabe-phone.gif",
+     "alt":"placeholder",
+     "text":"Sharing IG vids with each other."
+  },
+
+     "image63": {
+     "src":"https://media.tenor.com/S1CGBqNgumgAAAAC/snow-snowsuit.gif",
+     "alt":"placeholder",
+     "text":"You taking care of me by making sure I'm dressed warmly."
+  },
+
+     "image64": {
+     "src":"https://media.tenor.com/pdDPCkLF2vEAAAAC/naughty.gif",
+     "alt":"placeholder",
+     "text":"Ahhh! You're teasing me! Naughty naughty..."
+  },
+
+     "image65": {
+     "src":"https://media.tenor.com/RmiE2FnnvbEAAAAM/duplicate-soran-dussaigne.gif",
+     "alt":"placeholder",
+     "text":"Having two separate conversations on Whatsapp and Instagram."
+  },
+
+     "image66": {
+     "src":"https://media.tenor.com/lSPLOxPC3SIAAAAC/bandage-band-aid.gif",
+     "alt":"placeholder",
+     "text":"When you cleaned my wound after the first time we went skating."
+  }
+}
